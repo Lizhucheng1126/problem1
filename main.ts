8a05519e7314efc7e9defc5bf9a104a92b212816
Lizhucheng1126 <71689249+Lizhucheng1126@users.noreply.github.com>
2020-11-24 14:25:17 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,15 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showArrow(ArrowNames.East)
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showLeds(`
+        . . . . .
+        . # . # .
+        . . . . .
+        # . . . #
+        . # # # .
+        `)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showArrow(ArrowNames.West)
+})
